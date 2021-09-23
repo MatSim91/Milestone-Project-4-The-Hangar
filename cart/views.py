bc86fd343f67d5e5ee0b27a0992f9b27dfcf4463
MatSim91 <mateus.r.simoes@gmail.com>
2021-09-23 20:30:47 +0000

    Add adjust cart feature

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -21,3 +21,19 @@ def add_to_cart(request, item_id):
     
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+def adjust_cart(request, item_id):
+    """Update the shopping cart product quantity"""
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
