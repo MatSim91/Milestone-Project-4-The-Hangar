f23345733e28da309d93ce7fc3ffb5184ca62b9b
MatSim91 <mateus.r.simoes@gmail.com>
2021-09-26 19:53:34 +0000

    Add stripe

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Je361IvND6rEWdIJ9EaqSzcbcrZWfkDd8GgMNbptpUMpaaNPFluIlg5t533bNy6M8q47cFnuVKCJTvlOzBvaizB00fyS8qIi5',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
