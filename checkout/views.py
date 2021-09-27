cd0afbe5c932c2a446fac2c197532815fe18dfaa
MatSim91 <mateus.r.simoes@gmail.com>
2021-09-27 08:17:31 +0000

    Add form submit on cart checkout

--- checkout/views.py
+++ checkout/views.py
@@ -27,14 +27,17 @@ def checkout(request):
         currency=settings.STRIPE_CURRENCY,
     )
 
-    print(intent)
-
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51Je361IvND6rEWdIJ9EaqSzcbcrZWfkDd8GgMNbptpUMpaaNPFluIlg5t533bNy6M8q47cFnuVKCJTvlOzBvaizB00fyS8qIi5',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
