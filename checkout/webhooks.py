6f372381d6eb906013c8b72a3cd66640092d8fad
MatSim91 <mateus.r.simoes@gmail.com>
2021-10-01 13:48:13 +0000

    Fix webhooks not sending a response

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -7,8 +7,6 @@ from checkout.webhook_handler import StripeWH_Handler
 
 import stripe
 
-# Webhook built with Code Institute Help on the Miniproject
-
 @require_POST
 @csrf_exempt
 def webhook(request):
@@ -34,9 +32,6 @@ def webhook(request):
         return HttpResponse(status=400)
     except Exception as e:
         return HttpResponse(content=e, status=400)
-
-    print('Success!')
-    return HttpResponse(status=200)
 
     # Set up a webhook handler
     handler = StripeWH_Handler(request)
