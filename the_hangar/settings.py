e06eaba572f503cffcadd7c1c1e2e9818b4212b9
MatSim91 <mateus.r.simoes@gmail.com>
2021-10-05 21:18:46 +0000

    Update database to connect to heroku

--- the_hangar/settings.py
+++ the_hangar/settings.py
@@ -118,12 +118,18 @@ WSGI_APPLICATION = 'the_hangar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    print("Using Postgres")
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    print("Using SQLite")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 
 
 # Password validation
